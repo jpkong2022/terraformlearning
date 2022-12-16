e3290e2940ae19d67d33eb9626e1ad542a2448c3
jpkong2022 <99097145+jpkong2022@users.noreply.github.com>
2022-12-16 17:05:41 -0500

    update

--- simple/main.tf
+++ simple/main.tf
@@ -1,15 +1,4 @@
 terraform {
-backend  = "http" {
-address = "https://eu3-smax.saas.microfocus.com/iac-controller/v1/506666606/terraform/statejp/backend"
-lock_address = "https://eu3-smax.saas.microfocus.com/iac-controller/v1/506666606/terraform/statejp/lock"
-unlock_address = "https://eu3-smax.saas.microfocus.com/iac-controller/v1/506666606/terraform/statejp/lock"
-lock_method = "POST"
-unlock_method ="DELETE"
-username = "jean-patrick.kong@microfocus.com"
-password = "Automation_123"
-skip_cert_verification = "true"
-}
-address= "https://
   required_version = "~> 1.0" 
   required_providers {
     aws = {
